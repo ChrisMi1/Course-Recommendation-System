8109d43d8d5acecc85859a8e7bf82a6cf8ed733a
ChrisMi1 <xristosmalt@hotmail.com>
2025-05-01 13:54:02 +0300

    feat: initial commit

---
+++ src/test/java/com/uniwa/course_recommendation/CourseRecommendationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniwa.course_recommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseRecommendationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
